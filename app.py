a476c6f6d87edb9e54adfac39d4763159b5297f9
Pedro Cunha <>
2023-04-06 19:36:02 -0300

    version 1.0 with geolocation

--- app.py
+++ app.py
@@ -1,36 +1,28 @@
 from decouple import config
 import requests
+
 token = config('TOKEN')
 chat_code = config('CHAT_CODE')
+google_code = config('API_GOOGLE')
 
-   
-def get_data():
-    url = 'https://api.open-meteo.com/v1/forecast?latitude=-9.65&longitude=-35.73&hourly=temperature_2m,precipitation_probability,&forecast_days=1'
-    request = requests.get(url)
-    response = request.json()
-    last_temperature_index = len(response["hourly"]["temperature_2m"]) - 1
-    last_temperature = response["hourly"]["temperature_2m"][last_temperature_index]
-    last_preciptation_probability_index = len(response["hourly"]["precipitation_probability"]) -1 
-    last_preciptation_probability = response["hourly"]["precipitation_probability"][last_preciptation_probability_index]
-    return [last_temperature, last_preciptation_probability]
+def return_geolocation(city):
+    url = f"https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key={google_code}"
+    response = requests.get(url)
+    response = response.json()
+    latitude = response["results"][0]["geometry"]["location"]["lat"]
+    longitude = response["results"][0]['geometry']["location"]["lng"]
+    name = response["results"][0]["formatted_address"]
+    print(latitude, longitude, name)
+    return latitude, longitude, name
+
+def get_data(latitude, longitude):
+    urlz = f'https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m,precipitation_probability,&forecast_days=1'
+    responsez = requests.get(urlz)
+    responsez = responsez.json()
+    last_temperature_index = len(responsez["hourly"]["temperature_2m"]) - 1
+    last_temperature = responsez["hourly"]["temperature_2m"][last_temperature_index]
+    last_preciptation_probability_index = len(responsez["hourly"]["precipitation_probability"]) -1 
+    last_preciptation_probability = responsez["hourly"]["precipitation_probability"][last_preciptation_probability_index]
+    print(last_temperature, last_preciptation_probability)
+    return last_temperature, last_preciptation_probability
     
-def send_message():
-    url = f"https://api.telegram.org/bot{token}/sendMessage"
-    temperature, precipitation_chance = get_data()
-    text_ola = f"Olá!"
-    text_temperatura = f"A ultima temperatura registrada foi de {temperature}°C"
-    text_probabilidade = f"E a probabilidade de chuva está em {precipitation_chance}%"
-    params_ola = get_params(text_ola)
-    params_temperatura = get_params(text_temperatura)
-    params_probabilidade = get_params(text_probabilidade)
-    response = requests.get(url, params=params_ola)
-    response = requests.get(url, params=params_temperatura)
-    response = requests.get(url, params=params_probabilidade)
-
-def get_params(text):
-    params = {
-        "chat_id": chat_code,
-        "text": text,
-        "parse_mode": "HTML"
-    }
-    return params
--- main.py
+++ main.py
@@ -1,11 +1,39 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from app import send_message
-from time import sleep
+from telethon.sync import TelegramClient
+from telethon import events
+from app import *
+from decouple import config
+import asyncio
+from telethon import errors
 
-scheduler = BackgroundScheduler()
-scheduler.add_job(send_message, 'interval', hours=1)
+token = config('TOKEN')
+api_hash = config('API_HASH')
+api_id = config('API_ID')
 
-scheduler.start()
+try:
+    client = TelegramClient('anon', api_id, api_hash)
 
-while True:
-    sleep(1)
+    @client.on(events.NewMessage)
+    async def my_event_handler(event):
+        cidade = event.raw_text
+        try:
+            dados = return_geolocation(cidade)
+            await asyncio.sleep(1)
+            dados_1 = get_data(dados[0], dados[1])
+            await asyncio.sleep(1)
+            await event.reply(f"Olá, em {dados[2]} foram registrados:\nÚltima temperatura registrada foi de {dados_1[0]}°C\nE a probabilidade de chuva está em {dados_1[1]}%")
+        except Exception as e:
+            print(e)
+            await event.reply(f"Essa cidade não existe amigo, tente novamente")
+
+    client.start(bot_token=token)
+    client.run_until_disconnected()
+except errors.FloodWaitError as e:
+    # Wait for the specified number of seconds before retrying
+    wait_time = e.seconds
+    print(f"Got FloodWaitError, waiting for {wait_time} seconds...")
+    while wait_time > 0:
+        print(f"Got FloodWaitError, waiting for {wait_time} seconds...")
+        wait_time.sleep(30)
+        wait_time -= 30
+    print("Gotta!")
